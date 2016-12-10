3f35da02e2c04dc68853e8e58df6f22b9f195ee4
jftaggart02 <jftaggart02@gmail.com>
2016-12-09 17:41:35 -0700

    added other pages

--- js/app.js
+++ js/app.js
@@ -5,8 +5,27 @@ angular.module('app', ['ui.router'])
             .state('home', {
                 url: '/home',
                 templateUrl: 'templates/home.html'
+            })
+            .state('dinos', {
+                url: '/dinos',
+                templateUrl: 'templates/dinos.html'
+            })
+            .state('weapons', {
+                url: '/weapons',
+                templateUrl: 'templates/weapons.html'
+            })
+            .state('maps', {
+                url: '/maps',
+                templateUrl: 'templates/maps.html'
+            })
+            .state('mods', {
+                url: '/mods',
+                templateUrl: 'templates/mods.html'
             });
 
         $urlRouterProvider.otherwise('/home');
+
+    })
+    .controller('appCtrl', function($scope){
 
     });
